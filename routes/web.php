4376f04d96cac94a34cff930342c7254fe92044d
ilhamibnu <e41210178@student.polije.ac.id>
2023-05-25 00:18:31 +0700

    templating blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.login');
+});
+
+Route::get('/login', function () {
+    return view('admin.login');
+});
+
+Route::get('/index', function () {
+    return view('admin.pages.index');
+});
+
+Route::get('/dataayam', function () {
+    return view('admin.pages.dataayam');
+});
+
+Route::get('/datapakan', function () {
+    return view('admin.pages.datapakan');
+});
+
+Route::get('/dataovk', function () {
+    return view('admin.pages.dataovk');
+});
+
+Route::get('/datatenagakerja', function () {
+    return view('admin.pages.datatenagakerja');
+});
+
+Route::get('/datadistribusi', function () {
+    return view('admin.pages.datadistribusi');
+});
+
+Route::get('/datapengeluaran', function () {
+    return view('admin.pages.datapengeluaran');
+});
+
+Route::get('/datapendapatan', function () {
+    return view('admin.pages.datapendapatan');
 });
